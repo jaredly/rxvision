6afe4bec7a45b310d889c396bbb8a168a71da0de
Jared Forsyth <jared@jaredforsyth.com>
2015-03-14 16:55:45 -0600

    using webpack as build tool

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  // webpack options
+  entry: {
+    // the main angular module
+    'rxvision': './run/rx.js',
+    'viz': './run/viz.js',
+    'kefirvision': './run/kefir.js',
+    'playground': './run/playground.js',
+  },
+
+  node: {
+    fs: 'empty',
+    net: 'empty',
+  },
+
+  output: {
+    path: "./build",
+    // TODO(jared): think about using hashes to make caching a thing
+    filename: "[name].js",
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?optional=runtime' },
+      { test: /\.json$/, loader: 'json' },
+
+      {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+          test: /\.less$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      }
+    ],
+  },
+
+  plugins: [commonsPlugin],
+  devtool: 'eval',
+  colors: true,
+
+  plugins: [
+      new ExtractTextPlugin("[name].css")
+  ],
+}
+
